3b18490a759a06067a793959aaa8e93339ce0a81
martschneider <martin.a.schneider@accenture.com>
2016-06-28 13:53:26 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
-public class HelloWorld
-{
-	public static void main (String [] args)
-	{
-		System.out.println("qwertyqwerty");
-	}
-}
+ +public class HelloWorld
++{
++	public static void main (String [] args)
++	{
++		System.out.println("qwerty");
++	}
++}
